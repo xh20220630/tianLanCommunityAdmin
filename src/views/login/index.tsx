3c6242930945ad17a9161e78901accd6b069912f
xionghao <xionghao@yunquna.com>
2024-05-02 14:54:16 +0800

    🎉 init: 初始化天澜admin

---
+++ src/views/login/index.tsx
@@ -0,0 +1,28 @@
+import loginLeft from "@/assets/images/login_left.png";
+import logo from "@/assets/images/logo.png";
+import SwitchDark from "@/components/SwitchDark";
+import LoginForm from "./components/LoginForm";
+
+import "./index.less";
+
+const Login = () => {
+	return (
+		<div className="login-container">
+			<SwitchDark />
+			<div className="login-box">
+				<div className="login-left">
+					<img src={loginLeft} alt="login" />
+				</div>
+				<div className="login-form">
+					<div className="login-logo">
+						<img className="login-icon" src={logo} alt="logo" />
+						天澜社区
+					</div>
+					<LoginForm />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
